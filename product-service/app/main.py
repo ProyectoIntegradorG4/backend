ac133f8865584336f7c2aae50ea2d847f46ebe8f
Juan Pablo Rodriguez Morales <jp.rodriguezm@uniandes.edu.co>
2025-10-30 16:58:00 -0500

    Added product-service to aws deployment (#30)

--- product-service/app/main.py
+++ product-service/app/main.py
@@ -11,7 +11,7 @@ logger = logging.getLogger("uvicorn")
 app = FastAPI(
     title="Product Service (HU-WEB-003)",
     description="Microservicio de carga individual de productos médicos",
-    version="1.0.0"
+    version="1.1.0"
 )
 
 app.add_middleware(
