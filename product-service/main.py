104147137842285ce6ae9dd11ddb49207e905ca5
Juan Pablo Rodriguez Morales <jp.rodriguezm@uniandes.edu.co>
2025-10-30 17:52:25 -0500

    Fixing db issues

--- product-service/main.py
+++ product-service/main.py
@@ -11,7 +11,7 @@ logger = logging.getLogger("uvicorn")
 app = FastAPI(
     title="Product Service (HU-WEB-003)",
     description="Microservicio de carga individual de productos médicos",
-    version="1.1.1"
+    version="1.1.2"
 )
 
 app.add_middleware(
