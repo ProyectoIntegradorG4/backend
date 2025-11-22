286176c95287d53a357f4992e878a3a9ec68ca7b
Juan Pablo Rodriguez Morales <jp.rodriguezm@uniandes.edu.co>
2025-11-21 21:27:49 -0500

    Fixed pipeline

--- product-service/main.py
+++ product-service/main.py
@@ -15,7 +15,7 @@ logger = logging.getLogger("uvicorn")
 app = FastAPI(
     title="Product Service (HU-WEB-003 & HU-WEB-008)",
     description="Microservicio de productos médicos y planes de venta",
-    version="1.2.2"
+    version="1.2.3"
 )
 
 # Endpoints “legacy” sin prefijo
